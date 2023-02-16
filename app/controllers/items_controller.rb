5c0f66599a8ae70c9f1f6d622b8bc488c77a2dc1
qirpour <veikhegn@gmail.com>
2023-02-16 18:03:25 +0900

    ストロングパラメータ設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,9 +7,13 @@ class ItemsController < ApplicationController
   end
 
   def create
+    Item.create(item_params)
   end
 
   private
 
-  
+  def item_params
+    params.require(:item).permit(:title, :image, :explanation, :price, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :delivery_day_id).merge(user_id: current_user.id)
+  end
+
 end
