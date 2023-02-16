e78de57105d82b4448b4f3ab04fead3be2e8aa73
qirpour <veikhegn@gmail.com>
2023-02-16 17:28:33 +0900

    Active Storageの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,7 @@ class User < ApplicationRecord
     validates :last_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
     validates :first_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
   end
+
+  has_many :items
+  
 end
