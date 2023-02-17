41b6334886b1d8fb2b3abe3c8a25d644c9742575
qirpour <veikhegn@gmail.com>
2023-02-17 14:57:41 +0900

    parseIntメソッドからMath.floorメソッドへ変更

--- app/javascript/new.js
+++ app/javascript/new.js
@@ -2,10 +2,10 @@ function tax (){
   const itemPrice = document.getElementById("item-price")
   itemPrice.addEventListener("keyup", () => {
     const price = itemPrice.value;
-    const taxPrice = parseInt(price / 10)
+    const taxPrice = Math.floor(price / 10)
     const addTaxPrice = document.getElementById("add-tax-price")
     addTaxPrice.innerHTML = taxPrice ;
-    const profitPrice = parseInt(price - taxPrice)
+    const profitPrice = Math.floor(price - taxPrice)
     const profit = document.getElementById("profit")
     profit.innerHTML = profitPrice
   });
