c693bca81e93168b27005017b9be0873dcee16a9
qirpour <veikhegn@gmail.com>
2023-02-20 15:50:23 +0900

    orderコントローラのcreateアクション設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,12 +6,22 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.create(order_params)
+    Address.create(address_params)
   end
 
   private
 
+  def order_params
+    params.merge(user_id: current_user.id,item_id: params[:item_id])
+  end
+
+  def address_params
+    params.permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone).merge(order_id: @order.id)
+  end
+
   def move_to_index
-    @item = Item.find(params[:id])
+    @item = Item.find(params[:item_id])
     if current_user.id == @item.user.id
       redirect_to root_path
     end
