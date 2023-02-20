09eeb53dced4619931c407a6292b139a3c145acb
qirpour <veikhegn@gmail.com>
2023-02-20 16:20:34 +0900

    formオブジェクトの作成とvalidationの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,11 +1,4 @@
 class Address < ApplicationRecord
   belongs_to :order
-
-
-    validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
-    validates :telephone, presence: true, format: { with: /\A\d{10}$|^\d{11}\z/ , message: "is invalid."}
-    validates :city, presence: true
-    validates :house_number, presence: true
   
 end
---
+++ app/models/order_address.rb
@@ -0,0 +1,20 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone, :order_id, :user_id, :item_id
+
+  with_options presence: true do
+  validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  validates :telephone, format: { with: /\A\d{10}$|^\d{11}\z/ , message: "is invalid."}
+  validates :city
+  validates :house_number
+  validates :user_id
+  validates :item_id
+  end
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, telephone: telephone, order_id: order.id)
+  end
+
+end
