bbfb810bb59fd46d4e4a3d9c0150fc69d1a2632c
qirpour <veikhegn@gmail.com>
2023-02-21 22:20:29 +0900

    beforeアクションの整備　２

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,9 +2,9 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
   before_action :move_to_index, only: [:index]
   before_action :move_to_index_sold_out, only: [:index]
+  before_action :set_item
 
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
@@ -15,12 +15,15 @@ class OrdersController < ApplicationController
       @order_address.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render :index
     end
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
@@ -41,7 +44,6 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
-    @item = Item.find(params[:item_id])
     price = @item.price
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
