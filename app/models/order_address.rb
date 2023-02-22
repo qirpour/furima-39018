abd9d8824ea3016ef5d17543181a7cbb339e8c53
qirpour <veikhegn@gmail.com>
2023-02-22 17:52:10 +0900

    orderaddressモデルの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone, :order_id, :user_id, :item_id, :token
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone, :user_id, :item_id, :token
 
   with_options presence: true do
   validates :token
@@ -14,7 +14,7 @@ class OrderAddress
   end
 
   def save
-    order = Order.create(user_id: user_id, item_id: item_id, token: token)
+    order = Order.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, telephone: telephone, order_id: order.id)
   end
 
