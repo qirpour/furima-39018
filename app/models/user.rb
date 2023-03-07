c0a62f2c4b341c90921e7cef8f5df2edab663557
qirpour <veikhegn@gmail.com>
2023-03-08 00:08:44 +0900

    ymlファイルによるメッセージの翻訳

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :birthday, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
   with_options presence: true do
-    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters.' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'には全角文字を使用してください' }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'には全角カナを使用してください' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'には全角文字を使用してください' }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'には全角カナを使用してください' }
   end
+  validates :birthday, presence: true
 
   has_many :items
   has_many :orders
